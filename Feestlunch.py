69a50a4020e92a3022c5df21274aecfbf0302a34
Justin van Engelen <justinvanengelen@MacBook-Air-van-Justin.local>
2022-09-07 09:07:58 +0200

    feestlunch

---
+++ Feestlunch.py
@@ -0,0 +1,14 @@
+from importlib import import_module
+import math
+
+
+acroissant = 17
+croissant = 0.39 * acroissant
+
+astokbrood = 2
+stokbrood = 2.78 * astokbrood
+
+akorting= 3
+korting = 0.50 * akorting
+
+print(croissant+stokbrood-korting)
