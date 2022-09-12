5eaacbae9b4226a73249d7a25e19f7d75a6b96ca
Justin van Engelen <justinvanengelen@MacBook-Air-van-Justin.local>
2022-09-12 14:14:10 +0200

    else-statement

---
+++ else-statement.py
@@ -0,0 +1,13 @@
+a=input ("getal: ")
+b=input ("getal: ")
+if a>b : 
+    Max = a
+    print("a is het grootste getal: ",Max,"")
+
+elif a<b :
+    min=a
+    print("a is het kleinste getal ",min,"")
+
+else
+a==b
+print ("a en b zijn even groot")
