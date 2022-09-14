bb7bb82ed93347a0ce4487070fea5bfe7a291c33
Justin van Engelen <justinvanengelen@MacBook-Air-van-Justin.local>
2022-09-14 09:14:08 +0200

    sollicitatiecircus

---
+++ solicitatie.py
@@ -0,0 +1,32 @@
+print("+++++++++++++++++++++++++++++++++++++++++++")
+print ("er word een aantal relevante vragen gesteld")
+print("hier komen de vragen")
+print("++++++++++++++++++++++++++++++++++++++++++++")
+vraag1 = input("wat is uw naam?")
+vraag2 = input ("bent u een maan of vrouw?")
+vraag4 = input ("hoeveel jaar praktijkervaring met jongleren heeft u?")
+a=(vraag4)
+a = int (a)
+b= 4
+if a>b :
+            
+    vraag6 = input ("bent u n bezit van een Diploma MBO-4? j/n")
+if vraag6 == "j" :
+            vraag7 = input ("heeft u een hoge hoed? j/n")
+            if vraag7 == "j" :
+                vraag8 = input ("heeft u een Snor? j/n")
+                if vraag8 == "j" :
+                     vraag10 = input ("wat is uw lengte?")
+                     a=(vraag10)
+                     a= int (a)
+                     b=150
+                     if a>=b :
+                        vraag11 = input ("wat is uw lichamsgewicht?")
+                        a=(vraag11) 
+                        a= int (a)
+                        b=90
+                        if a>b :
+                            vraag12 = input ("Heeft u het Certificaat Overleven met gevaarlijk personeel?j/n")
+                            if vraag12 == "j" :
+
+                             print("balls")
