7192cb3b5377bc3e0dc661349b09a169f9dfa83d
Justin van Engelen <justinvanengelen@MacBook-Air-van-Justin.local>
2022-09-28 12:16:13 +0200

    tijd2.py

---
+++ Tijd2.py
@@ -0,0 +1,19 @@
+am = True
+pm = True
+tijd = 0
+tijd = int(tijd)
+
+while am == True:
+    print ("",tijd,"AM")
+    tijd = tijd + 1
+    if tijd <= 12:
+        am = True
+    else:
+        am=False
+while pm == True:
+    print("",tijd,"PM")
+    tijd = tijd + 1
+    if tijd <= 24:
+        pm = True
+    else:
+        pm = False
