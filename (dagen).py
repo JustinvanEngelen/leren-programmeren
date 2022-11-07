75d26a0853900211f22499878c60fd9670370df4
Justin van Engelen <justinvanengelen@MacBook-Air-van-Justin.local>
2022-11-07 13:54:41 +0100

    (dagen)

---
+++ (dagen).py
@@ -0,0 +1,10 @@
+from audioop import reverse
+
+
+thistuple=["maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag", "zondag"]
+print(thistuple[0:5:])
+print(thistuple[5:7])
+thistuple.reverse()
+print(thistuple)
+print(thistuple[0:2])
+print(thistuple[2:7])
---
+++ m&ms.py
@@ -0,0 +1,12 @@
+import random
+
+thistuple = [",oranje," ",blauw," ",groen," ",bruin,"]
+aantal = input ("hoeveel m&ms moeten er toegevoegd worden aan de zak")
+var1=[]
+for x in thistuple:
+    for x in range (aantal*2):
+        var1.append(aantal)
+random.shuffle(var1)
+
+
+
