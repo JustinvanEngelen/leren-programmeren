e9e01950e53d7695222ec70abe60e3fe2634155a
JustinvanEngelen <99074835@mydavinci.nl>
2022-11-14 15:29:52 +0100

    Update m&ms.py

--- m&ms.py
+++ m&ms.py
@@ -1,12 +1,9 @@
 import random
 
-thistuple = [",oranje," ",blauw," ",groen," ",bruin,"]
-aantal = input ("hoeveel m&ms moeten er toegevoegd worden aan de zak")
-var1=[]
-for x in thistuple:
-    for x in range (aantal*2):
-        var1.append(aantal)
-random.shuffle(var1)
-
-
-
+lijst =[]
+thistuple=["oranje", "blauw", "groen", "bruin"]
+aantal = input ("hoeveel M&Ms moet er toegevoegd worden")
+aantal=int(aantal)
+for x in range (aantal):
+    lijst.append(random.choices(thistuple))
+print(lijst)
