5155aee18645f55d3bc5eb583eeb368e951ab093
JustinvanEngelen <99074835@mydavinci.nl>
2022-12-07 12:02:07 +0100

    update

--- Module 4/Fruitmand/opdracht10.py
+++ Module 4/Fruitmand/opdracht10.py
@@ -1,7 +1,8 @@
 from fruitmand import fruitmand
 lijst = {}
 for i in range(len(fruitmand)):
-    lijst.update({fruitmand[i].get("weight"): fruitmand[i].get("name")})
+    lijst.update({fruitmand[i].get("weight")/1000: fruitmand[i].get("name")})
 for i in (fruitmand):
     lijst.update({i.get("weight"): i.get("name")})
+
 print(sorted(lijst.items(), reverse=True))
