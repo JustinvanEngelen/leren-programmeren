97990450260c563b916a7e57a5e66f355ef41166
JustinvanEngelen <99074835@mydavinci.nl>
2023-01-09 15:30:08 +0100

    Update

--- Module 5/Hello.py
+++ Module 5/Hello.py
@@ -1,4 +1,6 @@
-def showwelcome():
-    print("Hello from function town")
-showwelcome()
-    
+def showwelcome(welcometo:str,aantal:int):
+    for i in range(aantal):
+        print("Hello from function "+welcometo+str(i+1)+"!")
+where ='town '
+aantal = int(input("Hoe veel keer wilt je dit printen"))
+showwelcome(where,aantal)
---
+++ Module 5/tafels.py
@@ -0,0 +1,6 @@
+import math
+def tafels(welk:int):
+    welk = input("welk getal wilt ju de tafel van? : ")
+    for x in range (10):
+        print(welk,"x" ,x )
+tafels()
