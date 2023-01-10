7ef7cc860913ba4019aec03b4c9f21210bc14ec6
JustinvanEngelen <99074835@mydavinci.nl>
2023-01-10 11:51:01 +0100

    Tafels

--- Module 5/tafels.py
+++ Module 5/tafels.py
@@ -1,6 +1,16 @@
 import math
-def tafels(welk:int):
-    welk = input("welk getal wilt ju de tafel van? : ")
-    for x in range (10):
-        print(welk,"x" ,x )
-tafels()
+loop =True
+def tafels(getal:int):
+
+    for x in range (11):
+        antwoord = x*getal
+        print(x, "x" ,getal, "=", antwoord )
+while loop == True:
+    try:
+        getal = int(input("welk getal wilt u de tafel van : "))
+        loop = False
+    except:
+        print("dat is geen getal")
+        continue
+
+tafels(getal)
